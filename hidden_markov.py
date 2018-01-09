e88d66e3d1190af6ccb6a72fe9d3837ac42d8fa2
dogestoyevsky <dogestoyevsky@gmail.com>
2018-01-08 16:35:32 -0800

    Created function to produce transition matrix in hidden markov

---
+++ hidden_markov.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Jan  8 13:44:38 2018
+
+@author: hollymandel
+"""
+
+import numpy as np
+import random as rd
+
+def makeTransMat(sz):
+    # ord is the order of action - reciprocal to period
+    trans = np.random.rand(sz,sz)
+    trans = np.asmatrix(trans)
+    trans = np.asmatrix(trans)
+    trans_rowSums = trans*np.matrix.transpose(np.matrix(np.ones(sz)))
+    trans_rowSums = np.asarray(np.matrix.transpose(trans_rowSums))
+    trans_rowSumsInv = np.power(trans_rowSums,-1)
+    trans_rowSumsInv = np.asmatrix(np.diag(trans_rowSumsInv[0]))
+    trans = trans_rowSumsInv*trans
+    return trans
+
+samples = 10
+size1 = 5
+size2 = 3
+mapDown = np.random.randint(size2,size=(1,size1))
+states1 = np.random.rand(samples,size1)
+states2 = np.random.rand(samples,size2)
+trans1 = makeTransMat(size1)
+trans2 = makeTransMat(size2)
