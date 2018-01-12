e841694ca03caa8b9f7eabf9a6c70bb82d4aeb0a
hollymandel <holly.mandel@berkeley.edu>
2018-01-12 13:03:12 -0800

    fixed bug that occurs if not enough elements in each bin. But the only reason this is happening is because state choice is currently wrong (assumes normalized)

--- hidden_markov.py
+++ hidden_markov.py
@@ -10,42 +10,19 @@ import numpy as np
 import random as rd
 import matplotlib.pyplot as plt
 
-def makeTransMat(sz):
+def makeWeightMat(sz):
     # weights matrix with 0s on the diagonal (to match 4.3 in T-C)
     # symmetric. evaluate as input^t * P
     # it turns out that generating such a matrix that is also doubly stochastic
     # is nontrivial! So we are not normalizing here
-    trans = np.random.uniform(0,1,size=(sz,sz))
-    trans = trans + trans.T
-    trans = trans - np.diag(np.diag(trans))
-    
-#    trans = np.zeros((sz,sz))
-#    for i in range(0,sz):
-#        leftOver = 1-np.sum(trans[i,0:i])
-#        rowVals = np.random.rand(1,sz-(i+1)) # change distribution! no -!
-#        rowVals = leftOver*rowVals/np.sum(rowVals)
-#        trans[i] = np.concatenate((trans[i,0:i+1],rowVals[0]))
-#        trans[:,i] = trans[i].T
-#             
-             
-#    lastRowSum = np.sum(trans[:,sz-1])
-#    lastRowHold = trans[sz-1]/lastRowSum
-#    trans[sz-1] = lastRowHold
-#    trans[:,sz-1] = lastRowHold
-        
-#    trans = np.random.rand(sz,sz)
-#    transDiag = np.diag(np.diag(trans))
-#    trans = trans - transDiag
-#    trans = np.asmatrix(trans)
-#    trans_rowSums = trans*np.matrix.transpose(np.matrix(np.ones(sz)))
-#    trans_rowSums = np.asarray(np.matrix.transpose(trans_rowSums))
-#    trans_rowSumsInv = np.power(trans_rowSums,-1)
-#    trans_rowSumsInv = np.asmatrix(np.diag(trans_rowSumsInv[0]))
-#    trans = trans_rowSumsInv*trans
-    return trans
+    weight = np.random.uniform(0,1,size=(sz,sz))
+    weight = weight + weight.T
+    weight = weight - np.diag(np.diag(weight))
+
+    return weight
 
 def getStationaryDistribution(A):
-    # given an n x n transition matrix with 0s along the diagonal, returns the 
+    # given an n x n weightition matrix with 0s along the diagonal, returns the 
     # stationary distribution, computer as in T-C 4.3
     n = A.shape[1]
     W = sum(A)*np.transpose(np.matrix(np.ones(n)))
@@ -58,30 +35,16 @@ population = 10000
 samples = 15
 size1 = 5
 size2 = 3
+weight1 = makeWeightMat(size1)
+statDist = getStationaryDistribution(weight1)
 
-transCumulative = np.zeros(trans1.shape)
+weightCumulative = np.zeros(weight1.shape) # THIS IS WRONG
 for i in range(0,size1):
     for j in range(0,size1):
         for k in range(0,j+1):
-            transCumulative[i,j] += trans1[i,k]
+            weightCumulative[i,j] += weight1[i,k]
             
 mapDown = np.random.randint(size2,size=(size1))
-
-
-# prediction
-#p1 = getStationaryDistribution(trans1)
-#p2 = np.zeros(size2)
-#for i in range(0,size2):
-#    for j in range(0,size1):
-#        if mapDown[j] == i:
-#            p2[i] += p1[j]
-#        
-#print(p1)
-#print(mapDown)
-#print(p2)
-
-# simulation
-
         
 stateChoices = np.random.rand(samples-1,population)
 states = np.random.randint(0,size1, size=(1,population))
@@ -89,7 +52,7 @@ states = np.vstack((states[0],np.zeros((samples-1,population))))
 for i in range(1,samples):
     for j in range(0,population):
         getChoice = stateChoices[i-1,j]
-        getProb = transCumulative[states[i-1,j]]
+        getProb = weightCumulative[states[i-1,j]]
         probCheck = (getProb > getChoice)
         probCheck = np.nonzero(probCheck)
         states[i,j] = probCheck[0][0]
@@ -98,10 +61,13 @@ states = states.astype(int)
       
 fracStates = np.zeros((samples,size1))
 for i in range(0,samples):
-    fracStates[i] = np.bincount(states[i])
-        
-#fracStates = fracStates.cumsum(0)
-#fracNorms = population*(range(0,samples)+np.ones((1,samples)))
+    getBinCount = np.bincount(states[i])
+    gbSz = getBinCount.size
+    if gbSz < size1:
+        zeroPad = np.zeros((1,size1-gbSz))
+        getBinCount = np.concatenate((getBinCount,zeroPad[0]))                            
+    fracStates[i] = getBinCount
+    
 fracStates = fracStates / population
 
 
